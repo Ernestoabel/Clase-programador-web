e0c336c3c283e19d78c96821a4a1d32f469e3feb
Ernesto Abel Fatala <98593424+Ernestoabel@users.noreply.github.com>
2023-03-12 12:25:49 -0300

    Add files via upload

--- Clase 5 ejercicio 8 Ernesto Fatala.js
+++ Clase 5 ejercicio 8 Ernesto Fatala.js
@@ -2,29 +2,16 @@
 Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión
 medio. Realizar la misma actividad, pero ahora almacenando los valores en un array y
 luego recorriendolo para mostrar el resultado deseado del ejercicio. */
-let menUno='Vamos ';
-let menDos='San ';
-let menTres='Lorenzo ';
-let menCuatro='Ciclon ';
-let menCinco='Ponga ';
-let menSeis='Huvo ';
-let menSiete='cancelar ';
-let menOcho='Quiero ser campeon, soy de boedo';
-let mensaje='';
-let bandera;
-let booleano;
+let mensaje;
+let guion='-';
+let frase='';
 do{
-    booleano=mensaje.includes(menSiete);
-    if(booleano!=true){
-        bandera=1;
-    }else{
-    mensaje=menUno+menDos;
-    mensaje+=menTres;
-    mensaje+=menCuatro;
-    mensaje+=menCinco;
-    mensaje+=menSeis;
-    mensaje+=menSiete;
-    mensaje+=menOcho;
+    mensaje=prompt("Ingrse una palabra de la cadena: ");
+    if(mensaje!='cancelar'){
+        frase+=mensaje;
+        frase+=guion;
+        console.log(frase);
     }
-}while(booleano!=true);
-console.log(mensaje);
+}while(mensaje.includes("cancelar")!=true);
+    
+console.log(frase);
